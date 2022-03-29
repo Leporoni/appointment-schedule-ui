6935a70c3292ccb41872087299b2528c6c91cabf
Leporoni <leporoni@hotmail.com>
2022-03-29 14:21:51 -0300

    Added card to renderizing Region informations

--- src/app/incidences/incidences.module.ts
+++ src/app/incidences/incidences.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatSortModule} from '@angular/material/sort';
+import {MatCardModule} from '@angular/material/card';
 
 import { IncidencesRoutingModule } from './incidences-routing.module';
 import { IncidencesComponent } from './incidences/incidences.component';
@@ -13,7 +14,8 @@ import { IncidencesComponent } from './incidences/incidences.component';
   imports: [
     CommonModule,
     IncidencesRoutingModule,
-    MatSortModule
+    MatSortModule,
+    MatCardModule
   ]
 })
 export class IncidencesModule { }
