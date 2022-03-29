4a5549db70096b8193e9aa363186a731d02cb9ea
Leporoni <leporoni@hotmail.com>
2022-03-29 09:49:30 -0300

    Initial configs, created Incidence component and incidences routing and add mat toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IncidencesComponent } from './incidences/incidences/incidences.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IncidencesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/incidences/incidences.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { IncidencesRoutingModule } from './incidences-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    IncidencesRoutingModule
+  ]
+})
+export class IncidencesModule { }
